86b74e93cb41956a673ad18dea4e0ec96b42088b
John  Furr <jfurr@Johns-MacBook-Pro-2.local>
2025-07-09 11:06:30 -0400

    Cool little wave form

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -5,7 +5,24 @@
         Volatility is Vitality
     </p>
 
-    <!-- <p class="tagline">Volatility is Vitality</p> -->
+    <svg class="ekg-line" viewBox="0 0 500 100" preserveAspectRatio="none">
+      <polyline 
+        fill="none"
+        stroke="#f7931a"
+        stroke-width="3"
+        points="0,50 20,50 25,30 35,70 45,50 60,50 70,20 80,80 90,50 500,50"
+      >
+        <animate attributeName="points"
+          dur="2s"
+          repeatCount="indefinite"
+          values="
+            0,50 20,50 25,30 35,70 45,50 60,50 70,20 80,80 90,50 500,50;
+            0,50 20,50 25,70 35,30 45,50 60,50 70,80 80,20 90,50 500,50;
+            0,50 20,50 25,30 35,70 45,50 60,50 70,20 80,80 90,50 500,50
+          "
+        />
+      </polyline>
+    </svg>
   </div>
 </template>
 
@@ -51,5 +68,18 @@
     transform: scale(1);
     opacity: 1;
   }
+}
+
+@keyframes pulse {
+  0% { opacity: 1; }
+  50% { opacity: 0.6; }
+  100% { opacity: 1; }
+}
+
+.ekg-line {
+  width: 100%;
+  max-width: 500px;
+  height: 100px;
+  margin-top: 20px;
 }
 </style>
