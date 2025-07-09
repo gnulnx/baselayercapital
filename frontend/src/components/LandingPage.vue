f218e20950dfdc0c6798c4fb0a6b7be9cc56048a
John  Furr <jfurr@Johns-MacBook-Pro-2.local>
2025-07-09 11:04:33 -0400

    LandingPage coming togetehr

---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="landing-container">
+    <img src="@/assets/imgs/BaseLayerCapital.png" alt="Base Layer Capital Logo" class="logo" />
+    <p class=" tagline">
+        Volatility is Vitality
+    </p>
+
+    <!-- <p class="tagline">Volatility is Vitality</p> -->
+  </div>
+</template>
+
+<style scoped>
+.landing-container {
+  position: fixed;
+  inset: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+  padding: 2rem;
+  box-sizing: border-box;
+}
+
+.logo {
+  max-width: 300px;
+  width: 80%;
+  height: auto;
+}
+
+.tagline {
+  margin-top: 1.5rem;
+  font-size: 1rem;
+  font-weight: 600;
+  color: #f7931a;
+  animation: pulse 2.5s infinite;
+  margin-top: -20px;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  50% {
+    transform: scale(1.05);
+    opacity: 0.5;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import LandingPage from '@/components/LandingPage.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <LandingPage />
 </template>
+
